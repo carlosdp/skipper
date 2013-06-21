420d5501a9aabf62c1f1579da7e330a4742b98da
Carlos Diaz-Padron <cdpadron@twilio.com>
2013-06-21 11:47:04 -0700

    Added GitHub:FI

--- skipper.js
+++ skipper.js
@@ -8,11 +8,16 @@ var grabTree = function(root, current) {
     url: $(current).attr("href"),
     success: function(result) {
       objects = $('table.files tr td.content a', result);
+      if(objects.length < 1)
+      {
+        objects = $('#browser table tr td.content a', result);
+      }
+
       if(objects.length == 1)
       {
         grabTree(root, objects.first());
       }
-      else if(root != current && $(current).parent().parent().find('.icon span').hasClass('octicon-file-directory'))
+      else if(root != current && ($(current).parent().parent().find('.icon span').hasClass('octicon-file-directory') || $(current).parent().parent().find('.icon img').attr("alt") == 'directory'))
       {
         createLink(root, $(current).attr("href"));
       }
@@ -20,6 +25,12 @@ var grabTree = function(root, current) {
   });
 }
 
+// GitHub
 $('table.files tr td.content a').each(function() {
+  grabTree(this, this);
+});
+
+// GitHub:FI
+$('#browser table tr td.content a').each(function() {
   grabTree(this, this);
 });
