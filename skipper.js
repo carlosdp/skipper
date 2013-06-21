fb6d23a28c9aa93ae59dd46274da1694c3707c6c
Carlos Diaz-Padron <carlosdp@cmu.edu>
2013-06-21 13:51:07 -0700

    Encapsulated into local scope and added GitHub turbo-link detection

--- skipper.js
+++ skipper.js
@@ -1,36 +1,49 @@
-var createLink = function(root, path) {
-  $(root).parent().append("<a class='skipper-link' href='" + path + "'>skip -></a>");
-}
+$(document).ready(function(){
+  var createLink = function(root, path) {
+    $(root).parent().append("<a class='skipper-link' href='" + path + "'>skip -></a>");
+    $(root).attr("processing", null);
+  }
 
-var grabTree = function(root, current) {
+  var grabTree = function(root, current) {
 
-  $.ajax({
-    url: $(current).attr("href"),
-    success: function(result) {
-      objects = $('table.files tr td.content a', result);
-      if(objects.length < 1)
-      {
-        objects = $('#browser table tr td.content a', result);
+    $.ajax({
+      url: $(current).attr("href"),
+      success: function(result) {
+        objects = $('table.files tr td.content a', result);
+        if(objects.length < 1)
+        {
+          objects = $('#browser table tr td.content a', result);
+        }
+
+        if(objects.length == 1)
+        {
+          grabTree(root, objects.first());
+        }
+        else if(root != current && ($(current).parent().parent().find('.icon span').hasClass('octicon-file-directory') || $(current).parent().parent().find('.icon img').attr("alt") == 'directory'))
+        {
+          createLink(root, $(current).attr("href"));
+        }
       }
+    });
+  }
 
-      if(objects.length == 1)
+  var findLinks = function() {
+    // GitHub
+    $('table.files tr td.content a').each(function() {
+      // Processing flag to make sure we don't traverse this tree more than once
+      if($(this).parent().find('.skipper-link').length < 1 && $(this).attr("processing") != "1")
       {
-        grabTree(root, objects.first());
+        $(this).attr("processing", "1");
+        grabTree(this, this);
       }
-      else if(root != current && ($(current).parent().parent().find('.icon span').hasClass('octicon-file-directory') || $(current).parent().parent().find('.icon img').attr("alt") == 'directory'))
-      {
-        createLink(root, $(current).attr("href"));
-      }
-    }
-  });
-}
+    });
 
-// GitHub
-$('table.files tr td.content a').each(function() {
-  grabTree(this, this);
-});
+    // GitHub:FI
+    $('#browser table tr td.content a').each(function() {
+      grabTree(this, this);
+    });
+  }
 
-// GitHub:FI
-$('#browser table tr td.content a').each(function() {
-  grabTree(this, this);
+  $(this).bind('DOMNodeInserted', findLinks);
+  findLinks();
 });
