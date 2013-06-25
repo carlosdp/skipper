6c2f229dee1ea389b0d4faf0c955d5a69c57bbed
Carlos Diaz-Padron <carlosdp@cmu.edu>
2013-06-25 00:41:58 -0700

    Refactor directory check and add check to initial traversal

--- skipper.js
+++ skipper.js
@@ -5,6 +5,10 @@
       $(root).attr("processing", null);
     }
 
+    var isDirectory = function(element) {
+      return ($(element).parent().parent().find('.icon span').hasClass('octicon-file-directory') || $(element).parent().parent().find('.icon img').attr("alt") == 'directory');
+    }
+
     var grabTree = function(root, current) {
 
       $.ajax({
@@ -20,7 +24,7 @@
           {
             grabTree(root, objects.first());
           }
-          else if(root != current && ($(current).parent().parent().find('.icon span').hasClass('octicon-file-directory') || $(current).parent().parent().find('.icon img').attr("alt") == 'directory'))
+          else if(root != current && isDirectory(current))
           {
             createLink(root, $(current).attr("href"));
           }
@@ -32,7 +36,7 @@
       // GitHub
       $('table.files tr td.content a').each(function() {
         // Processing flag to make sure we don't traverse this tree more than once
-        if($(this).parent().find('.skipper-link').length < 1 && $(this).attr("processing") != "1")
+        if($(this).parent().find('.skipper-link').length < 1 && $(this).attr("processing") != "1" && isDirectory(this))
         {
           $(this).attr("processing", "1");
           grabTree(this, this);
@@ -41,7 +45,10 @@
 
       // GitHub:FI
       $('#browser table tr td.content a').each(function() {
-        grabTree(this, this);
+        if(isDirectory(this))
+        {
+          grabTree(this, this);
+        }
       });
     }
 
